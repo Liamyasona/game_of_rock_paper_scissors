673e86a4060f11add6268aaa95cd68142a35611f
unknown <liamkyleyasona@gmail.com>
2024-10-03 23:40:06 +0800

    Code complete

--- game_of_rock_paper_scissors.py
+++ game_of_rock_paper_scissors.py
@@ -10,12 +10,12 @@ while True:
     if user_input == "q": 
         break
 
-    if user_input not in options:
+    if user_input not in user_options:
         continue
 
     random_number = random.randint(0, 2)
     # rock: 0, paper: 1, scissors: 2
-    computer_pick = options[random_number]
+    computer_pick = user_options[random_number]
     print("Computer picked", computer_pick + ".")
 
     if user_input == "rock" and computer_pick == "scissors":
@@ -29,6 +29,10 @@ while True:
     elif user_input == "scissors" and computer_pick == "paper":
         print("You won!")
         user_wins += 1
+
+    else:
+        print("You lost!")
+        computer_wins += 1
 
 print("You won!", user_wins, "times.")
 print("The computer won!", computer_wins, "times.")
