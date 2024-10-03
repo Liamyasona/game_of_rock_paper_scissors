8cbf3ac44b360e7526fda4c40c48be05739cd01f
unknown <liamkyleyasona@gmail.com>
2024-10-03 23:43:26 +0800

    last revision

--- game_of_rock_paper_scissors.py
+++ game_of_rock_paper_scissors.py
@@ -34,6 +34,7 @@ while True:
         print("You lost!")
         computer_wins += 1
 
-print("You won!", user_wins, "times.")
-print("The computer won!", computer_wins, "times.")
-print("Goodbye!")
+print("You won!", "Your score:", user_wins)
+print("The computer won!", "Computer score:", computer_wins)
+print("Hope you had fun!")
+print("Play again!")
