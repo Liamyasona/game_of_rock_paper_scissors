2038fae4fd58fa4eb6b392650af546a07a45e430
unknown <liamkyleyasona@gmail.com>
2024-10-03 23:19:02 +0800

    second commit: added some variables

--- game_of_rock_paper_scissors.py
+++ game_of_rock_paper_scissors.py
@@ -1,1 +1,12 @@
 import random
+
+user_wins = 0
+computer_wins = 0
+
+user_options = ["rock", "paper", "scissors"]
+
+while True:
+    user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
+    if user_input == "q": 
+        break
+    
