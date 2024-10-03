3c1848a1db12f2bea80c456ea007efddf10cd905
unknown <liamkyleyasona@gmail.com>
2024-10-03 23:26:42 +0800

    more variables for the game

--- game_of_rock_paper_scissors.py
+++ game_of_rock_paper_scissors.py
@@ -9,4 +9,11 @@ while True:
     user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
     if user_input == "q": 
         break
+
+    if user_input not in options:
+        continue
     
+    random_number = random.randint(0, 2)
+    # rock: 0, paper: 1, scissors: 2
+    computer_pick = options[random_number]
+    print("Computer picked", computer_pick + ".")
